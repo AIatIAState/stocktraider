fc41000c9a4c4017b9bd872f9ee375c8f87215cf
Ethan Gruening <ethgru@iastate.edu>
2026-02-04 14:14:35 -0600

    Allowed the host in vite config and transfers database .db file

--- stockai-web/vite.config.ts
+++ stockai-web/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    preview: {
+        allowedHosts: [
+            'coms-4020-002.class.las.iastate.edu'
+        ]
+    },
   plugins: [
     react({
       babel: {
