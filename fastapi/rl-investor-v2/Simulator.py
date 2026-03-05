d8945fc4d1e027b5b3d6b37466ac614e5019e00d
Ethan Gruening <ethgru@iastate.edu>
2026-03-05 11:44:46 -0600

    Update to run trading strategies and output portfolio yearly earnings
    2019 - Portfolio 0: 140198.37765939615
    2019 - Portfolio 1: 141533.09069010976
    2019 - Portfolio 2: 141591.11641030834
    2019 - S&P500: 131490
    2020 - Portfolio 0: 138051.50221467487
    2020 - Portfolio 1: 138896.02396384868
    2020 - Portfolio 2: 138759.8915733211
    2020 - S&P500: 118400
    2021 - Portfolio 0: 126616.52170630035
    2021 - Portfolio 1: 124601.41993681974
    2021 - Portfolio 2: 124601.41993681974
    2021 - S&P500: 128710
    2022 - Portfolio 0: 91019.20810616524
    2022 - Portfolio 1: 90635.22373509
    2022 - Portfolio 2: 90611.41645863018
    2022 - S&P500: 91990
    2023 - Portfolio 0: 126289.63480879254
    2023 - Portfolio 1: 125160.06213323293
    2023 - Portfolio 2: 125159.60927361321
    2023 - S&P500: 126290
    2024 - Portfolio 0: 111504.54729605583
    2024 - Portfolio 1: 112809.63304790517
    2024 - Portfolio 2: 112807.50756049894
    2024 - S&P500: 125020
    2025 - Portfolio 0: 115367.21463524253
    2025 - Portfolio 1: 114135.62050037917
    2025 - Portfolio 2: 113890.7532773249
    2025 - S&P500: 117880

--- fastapi/rl-investor-v2/Simulator.py
+++ fastapi/rl-investor-v2/Simulator.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-from datetime import date
+from datetime import date, timedelta
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -97,6 +97,76 @@ def simulate_year(year, look_back=5, precompute=True, save_dir= "./"):
         "Corr": float(np.corrcoef(y_test, y_preds)[0, 1]),
     })
 
+def trade(year, save_dir="./"):
+    predictions = pd.read_csv(f"{save_dir}predictions_{year}.csv")
+    predictions = predictions.sort_values(by="dates")
+
+    starting_portfolio_value = 100000
+    starting_date = date(year, 1, 1)
+    ending_date = date(year + 1, 1, 1)
+
+
+    portfolio1 =[(starting_date, starting_portfolio_value)]
+    n = 30
+
+    portfolio2 = [(starting_date, starting_portfolio_value)]
+    min_return = .005
+    max_investments = 30
+
+    portfolio3 = [(starting_date, starting_portfolio_value)]
+
+
+    current_day = starting_date
+
+
+    while current_day < ending_date:
+        todays_predictions = predictions[predictions['dates'] == current_day.strftime("%Y-%m-%d")]
+        if todays_predictions.empty:
+            current_day += timedelta(days=1)
+            continue
+
+        # Strategy 1: Equal weights on top n predictions (ignores confidence)
+        top_n_prediction_rows = todays_predictions.nlargest(n, 'y_pred')
+        true_returns = np.array(top_n_prediction_rows['y_true'].values) + 1
+        new_portfolio_value = (true_returns * (portfolio1[-1][1] / n)).sum()
+        portfolio1.append((current_day, new_portfolio_value))
+
+        # Strategy 2: Equal weights with threshold
+        candidates = todays_predictions[todays_predictions['y_pred'] > min_return]
+        if not candidates.empty:
+            candidates = candidates.nlargest(max_investments, 'y_pred')
+            true_returns = np.array(candidates['y_true'].values) + 1
+            new_portfolio_value = (true_returns * (portfolio1[-1][1] / len(true_returns))).sum()
+            portfolio2.append((current_day, new_portfolio_value))
+
+        #Strategy 3: Weighted top predictions
+        candidates = todays_predictions[todays_predictions['y_pred'] > min_return]
+        if not candidates.empty:
+            candidates = candidates.nlargest(max_investments, 'y_pred')
+            weights = np.array(candidates['y_pred'] / candidates['y_pred'].sum())
+            true_returns = np.array(candidates['y_true'].values) + 1
+            new_portfolio_value = (true_returns * (weights * portfolio1[-1][1])).sum()
+            portfolio3.append((current_day, new_portfolio_value))
+
+
+
+
+        current_day += timedelta(days=1)
+
+    portfolios = [portfolio1, portfolio2, portfolio3]
+    sp_returns = {2019: 131490, 2020: 118400, 2021: 128710, 2022: 91990, 2023: 126290, 2024: 125020, 2025: 117880}
+    print(f"{year} - S&P500: {sp_returns[year]}")
+    for i, portfolio in enumerate(portfolios):
+        print(f"{year} - Portfolio {i}: {portfolio[-1][1]}")
+
+
+
+    return portfolios
+
+
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--lookback", type=int, default=5)
@@ -107,4 +177,5 @@ if __name__ == "__main__":
     years = [2019, 2020, 2021, 2022, 2023, 2024, 2025]
     for year in years:
         simulate_year(year, look_back=args.lookback, precompute=args.use_precomputed_df, save_dir="./prediction_simulation/")
+        trade(year, save_dir="./prediction_simulation/")
 
